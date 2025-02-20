a8e84fbc7147e09075c0bb237cacd11cbed303a9
Rens Gerritsen <gerritsenREN2@gmail.com>
2025-02-20 12:12:44 +0100

    example html

--- index.py
+++ index.py
@@ -5,28 +5,28 @@ from bokeh.plotting import figure, show
 
 
 
-import pandas as pd
-data = pd.read_csv('./data/reviews_202106.csv')
-data.head() # to display the first 5 lines of loaded data
+# import pandas as pd
+# data = pd.read_csv('./data/reviews_202106.csv')
+# data.head() # to display the first 5 lines of loaded data   
 
 
 
-# p = figure(title="Bokeh Markers", toolbar_location=None)
-# p.grid.grid_line_color = None
-# p.background_fill_color = "#eeeeee"
-# p.axis.visible = False
-# p.y_range.flipped = True
+p = figure(title="Bokeh Markers", toolbar_location=None)
+p.grid.grid_line_color = None
+p.background_fill_color = "#eeeeee"
+p.axis.visible = False
+p.y_range.flipped = True
 
-# N = 10
+N = 10
 
-# for i, marker in enumerate(MarkerType):
-#     x = i % 4
-#     y = (i // 4) * 4 + 1
+for i, marker in enumerate(MarkerType):
+    x = i % 4
+    y = (i // 4) * 4 + 1
 
-#     p.scatter(random(N)+2*x, random(N)+y, marker=marker, size=14,
-#               line_color="navy", fill_color="orange", alpha=0.5)
+    p.scatter(random(N)+2*x, random(N)+y, marker=marker, size=14,
+              line_color="navy", fill_color="orange", alpha=0.5)
 
-#     p.text(2*x+0.5, y+2.5, text=[marker],
-#            text_color="firebrick", text_align="center", text_font_size="13px")
+    p.text(2*x+0.5, y+2.5, text=[marker],
+           text_color="firebrick", text_align="center", text_font_size="13px")
 
-# show(p)
+show(p)
